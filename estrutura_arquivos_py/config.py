ef62fd1f0e267d5033dce5aa1e75932bd6cf9f38
joaobuso <61099397+joaobuso@users.noreply.github.com>
2024-02-21 18:03:41 -0300

    Atualização Template

--- estrutura_arquivos_py/config.py
+++ estrutura_arquivos_py/config.py
@@ -2,8 +2,10 @@ import os
 
 ## Inserir registros de configuração do Bot
 
-#TOKEN API HYPERCOE
-ClientToken = '<Capturar token gerado pelo portal HyperCoe no painel do usuário>'
+
+# Alterar o Kill Process de acordo o processo que deverá ser encerrado
 Kill_Process = {'EXCEL.EXE','firefox.exe'}
+
+# Não alterar os parâmetros abaixo
 bibliotecas = ["requests", "psutil", "pyautogui","rpa-hypercoe","jsonpickle"]
 path = fr"{os.path.dirname(os.path.dirname(os.path.abspath(__file__)))}\config.txt"
